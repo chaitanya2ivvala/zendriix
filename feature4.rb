9759cb79f71a7519f917df28a56d564407a93f0d
Chaitanya Krishna <chaitanyaivvala1996@gmail.com>
2019-10-19 22:02:55 +0200

    Added Feature4

---
+++ feature4.rb
@@ -0,0 +1,20 @@
+class Person
+    attr_reader :name, :email
+  
+    def initialize(name, email)
+      @name  = name
+      @email = email
+    end
+  
+    # Duplicate attr_readers.
+    # TODO: Remove these comments and the 2 methods below.
+    # git change
+    def name
+      @name
+    end
+  
+    def email
+      @email
+    end
+  end
+  
